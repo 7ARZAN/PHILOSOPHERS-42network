f7dd86eabaf0920b1980123cefb95c9563efc8ec
7ARZAN <7arzan@7ARZANs-MacBook-Air.local>
2023-05-30 17:30:43 +0100

    ALUMNI tarzan

--- src/include/philo.h
+++ src/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: 7arzan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 21:51:02 by 7arzan            #+#    #+#             */
-/*   Updated: 2023/05/29 18:17:44 by elakhfif         ###   ########.fr       */
+/*   Updated: 2023/05/30 17:30:29 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,23 +43,23 @@ typedef struct s_data
 }    t_data;
 
 
-void	status(char *str, t_philo *data, int i);
-void	take_forks(t_philo *data, int i);
-void	eat(t_philo *data, int i);
-void	sleeping_thinking(t_philo *data, int i);
+void	status(char *str, t_data *data, int i);
+void	take_forks(t_data *data, int i);
+void	eat(t_data *data, int i);
+void	sleeping_thinking(t_data *data, int i);
 int	check_meals(t_philo *philo);
 void	cleaner(t_philo *philo);
-void	main_checker(t_philo *data);
+void	main_checker(t_data *data);
 int	create_mutex(t_philo *philo);
 int	create_thread(t_philo *philo);
 int	ft_isdigit(int c);
 int	ft_atoi(const char *str);
 long	get_time(void);
 void	mssleep(int time);
-int	init(t_philo *data);
+int	init(t_data *data);
 int	philosopher_death(t_philo *philo);
-int	simulation(t_philo *data);
-int	check_arguments(char *args, int position, t_philo *data);
+int	simulation(t_data *data);
+int	check_arguments(char *args, int position, t_data *data);
 int	main(int ac, char **av);
 
 #endif
